5e77b275fbb45c9f601eeba2106700f89688f5cc
Prashat <prashat@ymail.com>
2020-02-10 21:06:33 -0500

    States created. Editor script for counting the number of pellets created.

---
+++ PacMan/Assets/Game/Scripts/Editor/PelletCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+
+public class PelletCounter : EditorWindow
+{
+    [MenuItem("Tools/Pellet Counter")]
+    [Shortcut("ShowPelletCount", KeyCode.F9)]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        PelletCounter window = (PelletCounter)EditorWindow.GetWindow(typeof(PelletCounter));
+        window.maxSize = new Vector2(200, 50);
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Pellets in Scene: ", EditorStyles.boldLabel);
+        GUILayout.Label("" + FindObjectsOfType(typeof(PelletController)).Length);
+
+        GUILayout.Label("Objects Selected: ", EditorStyles.boldLabel);
+        GUILayout.Label("" + ((Selection.objects.Length > 0) ? Selection.objects.Length.ToString() : "0"));
+
+    }
+
+    private void OnSelectionChange()
+    {
+        Repaint();
+    }
+}
